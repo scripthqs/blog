ffe83630bef09b572a6f3e5ed9fbe8e8ace48b31
Scripthqs <1941186772@qq.com>
2022-01-18 21:03:03 +0800

    2022-01-18 21:02:22

--- docs/.vuepress/components/collapse-demo.vue
+++ docs/.vuepress/components/collapse-demo.vue
@@ -26,16 +26,13 @@ export default {
             }
         <h-collapse :selected.sync="selectedTab">
             <h-collapse-item title="图片1" name="1">
-                <img height="445"
-                     src="./1.png">
+                <img height="445" src="./1.png">
             </h-collapse-item>
             <h-collapse-item title="图片2" name="2">
-                <img height="300px"
-                     src="./2.png">
+                <img height="300px" src="./2.png">
             </h-collapse-item>
             <h-collapse-item title="图片3" name="3">
-                <img height="440"
-                     src="./3.png">
+                <img height="440" src="./3.png">
             </h-collapse-item>
         </h-collapse>
       `.replace(/\s{2,}/, '').trim()
