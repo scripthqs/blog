c601a0185d5d24f8a9bca516018827cf1d12d018
unknown <scripthqu@gmaile.com>
2023-01-07 14:43:44 +0800

    a

--- docs/project/vuecms/sidebar.js
+++ docs/project/vuecms/sidebar.js
@@ -2,8 +2,11 @@ module.exports = [
   {
     title: '后台管理系统',
     collapsable: true,
-    children: [
-      '/project/vuecms/',
-    ]
+    children: ['/project/vuecms/']
+  },
+  {
+    title: 'ERP',
+    collapsable: true,
+    children: ['/project/vuecms/erp.md']
   }
-]
+];
