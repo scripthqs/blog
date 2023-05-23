2828a4d63aab550a0572e5865341219c66b5d3c6
scripthqs <1941186772@qq.com>
2023-05-24 00:37:50 +0800

    1

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -1,12 +1,5 @@
 export default [
   ["link", { rel: "icon", href: "/img/icon.png" }],
+  ["link", { rel: "stylesheet", href: "/style/index.css" }],
   ["meta", { name: "viewport", content: "width=device-width,initial-scale=1,user-scalable=no" }],
 ];
-
-// [
-//   ["link", { rel: "icon", href: "/img/icon.png" }],
-//   ["link", { rel: "stylesheet", href: "/style/index.css" }],
-//   ["link", { rel: "stylesheet", href: "/style/valine.css" }],
-//   ["link", { rel: "stylesheet", href: "/style/code.css" }],
-//   ["meta", { name: "viewport", content: "width=device-width,initial-scale=1,user-scalable=no" }],
-// ],
